b8702effa07e6d71f8ec243f642549bb99bf0af0
phelrine <s.ngskx615@gmail.com>
2012-02-23 13:18:15 +0900

    change indent style

--- config/cc-config.el
+++ config/cc-config.el
@@ -4,4 +4,6 @@
           (lambda ()
             (add-to-list 'ac-sources 'ac-source-semantic)
             (local-set-key (kbd "C-c c") 'compile)
+            (c-set-offset 'innamespace 0)
+            (c-set-offset 'arglist-close 0)
             (hs-minor-mode t)))
