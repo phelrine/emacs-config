f3752d14e845cceb26193c9107d845587852c891
phelrine <s.ngskx615@gmail.com>
2014-01-07 15:19:59 +0900

    sync tex

--- config/latex-mode-config.el
+++ config/latex-mode-config.el
@@ -19,9 +19,44 @@
 
   (push '("\\.tex$" flymake-tex-init flymake-tex-cleanup-custom) flymake-allowed-file-name-masks))
 
+(require 'dbus)
+
+(defun un-urlify (fname-or-url)
+  "A trivial function that replaces a prefix of file:/// with just /."
+  (if (string= (substring fname-or-url 0 8) "file:///")
+      (substring fname-or-url 7)
+    fname-or-url))
+
+(defun th-evince-sync (file linecol &rest ignored)
+  (let* ((fname (un-urlify file))
+         (buf (find-buffer-visiting fname))
+         (line (car linecol))
+         (col (cadr linecol)))
+    (if (null buf)
+        (message "[Synctex]: %s is not opened..." fname)
+      (switch-to-buffer buf)
+      (goto-line (car linecol))
+      (unless (= col -1)
+        (move-to-column col)))))
+
+(defvar *dbus-evince-signal* nil)
+
+(defun enable-evince-sync ()
+  (require 'dbus)
+  (when (and
+         (eq window-system 'x)
+         (fboundp 'dbus-register-signal))
+    (unless *dbus-evince-signal*
+      (setf *dbus-evince-signal*
+            (dbus-register-signal
+             :session nil "/org/gnome/evince/Window/0"
+             "org.gnome.evince.Window" "SyncSource"
+             'th-evince-sync)))))
+
 (add-hook 'latex-mode-hook
           '(lambda ()
              (reftex-mode 1)
-             (auto-fill-mode -1)))
+             (auto-fill-mode -1)
+             (enable-evince-sync)))
 
 (setq auto-mode-alist (cons '("\\.tex\\'" . latex-mode) auto-mode-alist))
