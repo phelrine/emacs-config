6460208ebd575c5271b582fb1eb062dab57822d3
phelrine <s.ngskx615@gmail.com>
2014-01-10 15:37:11 +0900

    latex用flymakeの設定

--- config/latex-mode-config.el
+++ config/latex-mode-config.el
@@ -1,25 +1,25 @@
-(when (require 'flymake nil t)
-  (defun flymake-tex-init ()
-    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                         'flymake-create-temp-inplace))
-           (local-dir   (file-name-directory buffer-file-name))
-           (local-file  (file-relative-name
-                         temp-file
-                         local-dir)))
-      (list "platex" (list "-file-line-error" "-interaction=nonstopmode" local-file))))
-
-  (defun flymake-tex-cleanup-custom ()
-    (let* ((base-file-name (file-name-sans-extension (file-name-nondirectory flymake-temp-source-file-name)))
-           (regexp-base-file-name (concat "^" base-file-name "\\.")))
-      (mapc '(lambda (filename)
-               (when (string-match regexp-base-file-name filename)
-                 (flymake-safe-delete-file filename)))
-            (split-string (shell-command-to-string "ls"))))
-    (setq flymake-last-change-time nil))
-
-  (push '("\\.tex$" flymake-tex-init flymake-tex-cleanup-custom) flymake-allowed-file-name-masks))
-
+(require 'flymake)
 (require 'dbus)
+
+(defun flymake-tex-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-dir   (file-name-directory buffer-file-name))
+         (local-file  (file-relative-name
+                       temp-file
+                       local-dir)))
+    (list "platex" (list "-file-line-error" "-interaction=nonstopmode" local-file))))
+
+(defun flymake-tex-cleanup-custom ()
+  (let* ((base-file-name (file-name-sans-extension (file-name-nondirectory flymake-temp-source-file-name)))
+         (regexp-base-file-name (concat "^" base-file-name "\\.")))
+    (mapc '(lambda (filename)
+             (when (string-match regexp-base-file-name filename)
+               (flymake-safe-delete-file filename)))
+          (split-string (shell-command-to-string "ls"))))
+  (setq flymake-last-change-time nil))
+
+(push '("\\.tex$" flymake-tex-init flymake-tex-cleanup-custom) flymake-allowed-file-name-masks)
 
 (defun un-urlify (fname-or-url)
   "A trivial function that replaces a prefix of file:/// with just /."
@@ -55,6 +55,7 @@
 
 (add-hook 'latex-mode-hook
           '(lambda ()
+             (flymake-mode 1)
              (reftex-mode 1)
              (auto-fill-mode -1)
              (enable-evince-sync)))
