cb624a1f0e334b69f2310a5ab80afcc8146c1336
phelrine <phelrine@gmail.com>
2023-08-27 21:55:45 +0900

    font 設定修正

--- init.el
+++ init.el
@@ -102,11 +102,12 @@
 (defalias 'message-box 'message)
 (when (eq (window-system) 'ns)
   (set-face-attribute 'default nil :family "Menlo" :height 180)
-  (set-fontset-font (frame-parameter nil 'font)
-                    'japanese-jisx0208
-                    (font-spec :family "Hiragino Kaku Gothic ProN"))
-  (add-to-list 'face-font-rescale-alist
-               '(".*Hiragino Kaku Gothic ProN.*" . 1.1)))
+  (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Hiragino Kaku Gothic ProN"))
+  (add-to-list 'face-font-rescale-alist '(".*Hiragino Kaku Gothic ProN.*" . 1.1)))
+(when (eq (window-system) 'x)
+  (set-face-attribute 'default nil :family "Inconsolata" :height 140)
+  (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP" :size 28)))
+
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
